90caf004dd2d2e130b1db33f2eeaa384fd48b682
Yuriya <135228172+YuriyaJP@users.noreply.github.com>
2023-12-21 11:14:58 +0900

    Add files via upload

--- PDF reader1.py
+++ PDF reader1.py
@@ -1,9 +1,11 @@
 import pyttsx3
 import PyPDF2
+from gtts import gTTS
+import os
 
-pdf_file_path = 'C:/Users/Yuliia/Downloads/pytorch_image_class/PDF_reader.pdf'
+pdf_file_path = r'C:\Users\julia\coding projects\speech production\PDF_reader.pdf'
 
-# Initialize the text-to-speech engine with the desired English voice
+# Initialize the text-to-speech engine
 speaker = pyttsx3.init()
 speaker.setProperty('voice', 'TTS_MS_EN-GB_HAZEL_11.0')
 
@@ -13,16 +15,19 @@ with open(pdf_file_path, 'rb') as file:
     pdfreader = PyPDF2.PdfReader(file)
 
     # Iterate through each page and extract text
+    full_text = ''
     for page_num in range(len(pdfreader.pages)):
         text = pdfreader.pages[page_num].extract_text()
         clean_text = text.strip().replace('\n', ' ')
         print(clean_text)
+        full_text += clean_text + '\n'
 
         # Speak the extracted text
         speaker.say(clean_text)
 
-    # Save the spoken text to an audio file (e.g., audio.mp3)
-    speaker.save_to_file(clean_text, 'audio.mp3')
+    # Save the spoken text to an audio file gTTS
+    tts = gTTS(text=full_text, lang='en')
+    tts.save('audio.mp3')
 
 # Wait for the speech to finish
 speaker.runAndWait()
